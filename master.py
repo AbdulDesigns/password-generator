545f843c0eaf37d5b0b1fec031a6935a424dd491
abdul <abduldesigns24@gmail.com>
2024-07-12 13:45:18 +0530

    added code for generator

--- master.py
+++ master.py
@@ -1,1 +1,13 @@
-print("this is password generator project")
+import random
+import string
+
+password_len = 10
+password = ""
+base_characters =  string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
+
+# looping over collection to generate random password
+for i in range(password_len):
+  password += random.choice(base_characters)
+
+print("generated random password:",password)  
+
