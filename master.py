26256e5d8a135f95a9cf5fe0b3d240ffaf511117
abdul <abduldesigns24@gmail.com>
2024-07-12 13:57:27 +0530

    added code with list comprehension method

--- master.py
+++ master.py
@@ -6,8 +6,11 @@ password = ""
 base_characters =  string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
 
 # looping over collection to generate random password
-for i in range(password_len):
-  password += random.choice(base_characters)
+# for i in range(password_len):
+#   password += random.choice(base_characters)
 
-print("generated random password:",password)  
+# print("generated random password:",password)  
 
+# using list comprehension method
+password = "".join([random.choice(base_characters) for i in range(password_len)])
+print(password)
